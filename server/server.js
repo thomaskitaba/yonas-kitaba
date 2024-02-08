8f10cbc13ebb5d417695eb922e58eb0776f8be04
thomaskitaba <thomas.kitaba@gmail.com>
2024-02-08 16:58:36 +0300

    started creating json for each views

--- server/server.js
+++ server/server.js
@@ -1,104 +1,138 @@
-const express = require('express');
-const cors = require('cors');
-const sqlite3 = require('sqlite3');
+  const express = require('express');
+  const cors = require('cors');
+  const sqlite3 = require('sqlite3');
 
-const app = express();
-const port = 5000;
+  const app = express();
+  const port = 5000;
 
-app.use(cors())
-// Create and initialize the SQLite database
-const db = new sqlite3.Database('./posts.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) return conole.error(err);
-});
-
-// db.serialize(() => {
-//   db.run(`create, update, insert`)    go here
-//  });
-// Endpoint for the root path
-
-//'/
-
-// TODO: GLOBAL VARIABLES
-const jsonInitialized = false;
-// general sql statments for use in enpoints
-const specficPosts = 'SELECT * FROM posts WHERE postStatus LIKE \"Active\" and postId LIKE ?';
-const allPostsSql = 'SELECT * FROM posts  WHERE postStatus LIKE \"Active\"';
-const allPostCommentsSql = 'SELECT * FROM postCommentsView';
-let allPostsJson = [];
-let allPostCommentsJson = [];
-
-const allPostsFunction = () => {
-  return new Promise((resolve, reject) => {
-    db.all(allPostsSql, (err, rows) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(rows);
-      allPostsJson.push(rows);
-
-    });
+  app.use(cors())
+  // Create and initialize the SQLite database
+  const db = new sqlite3.Database('./posts.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return conole.error(err);
   });
-};
 
-// function to get all post comments
-const allPostCommentsFunction = () => {
-  return new Promise ((resolve, reject) => {
-    db.all(allPostCommentsSql, (err, rows) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(rows);
+  // db.serialize(() => {
+  //   db.run(`create, update, insert`)    go here
+  //  });
+  // Endpoint for the root path
+
+  //'/
+
+  // TODO: GLOBAL VARIABLES
+  const jsonInitialized = false;
+  const all = [];
+  // general sql statments for use in enpoints
+  const specficPosts = 'SELECT * FROM posts WHERE postStatus LIKE \"Active\" and postId LIKE ?';
+  const allPostsSql = 'SELECT * FROM posts  WHERE postStatus LIKE \"Active\"';
+  const allPostCommentsSql = 'SELECT * FROM postCommentsView';
+
+  let allPostsJson = [];
+  let allPostCommentsComment = [];
+
+
+  let activeCommentsViewJson = [];
+  let activePostsCommentsView = []
+  let activePostsView = []
+  let activeRepliesView = []
+
+  let allPostCommentsJson = [];
+
+
+
+  //
+
+  const allPostsFunction = () => {
+    return new Promise((resolve, reject) => {
+      db.all(allPostsSql, (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(rows);
+
+      });
+    });
+  };
+
+  // function to get all post comments
+  const activePostsCommentsViewFunction = () => {
+    return new Promise ((resolve, reject) => {
+      db.all(allPostCommentsSql, (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(rows);
+      })
     })
-  })
-}
-
-app.get('/', async (req, res) => {
-  try {
-    const all = []
-    const posts = await allPostsFunction();
-    allPostsJson.push(posts) // for later use
-    all.push(posts);
-    const postCommentsView = await allPostCommentsFunction();
-    allPostCommentsJson.push(postCommentsView) // for later use
-    all.push(postCommentsView);
-
-    console.log(all)
-    res.json(all[1]);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
   }
-});
 
 
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  const activePostsCommentsViewFunction = () => {
+    return new Promise((resolve, reject) => {
+      db.all('SELECT * FROM  activePostsCommentsView', (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(rows);
+      });
+    });
+  }
 
+  // TODO: ==== NO AUTHORIZATION NEDED =====
 
+  app.get('/', async (req, res) => {
+    try {
+      const activeCommentsView = wait activePostsCommentsViewFunction();
+      all.push(activeCommentsView);
+    } catch (error) {
+      res.status(500).json({error: error.stack})
+    }
+  });
 
-// const pids = [1, 4];
-// const result = [];
-// let completedQueries = 0;
-// app.get('/', (req, res) => {
-//   for (let pid = 0; pid < pids.length; pid++) {
-//     db.all(specficPosts, pids[pid], (err, rows) => {
-//       completedQueries++;
+  app.get('/api/posts', async (req, res) => {
+    try {
+      const posts = await allPostsFunction();
+      allPostsJson.push(posts) // for later use
+      res.json(posts)
 
-//       if (err) {
-//         res.status(500).json({ error: err.message });
-//         return; // Exit the function early to avoid further processing
-//       }
+    } catch (error) {
+      res.status(500).json({ error: error.stack });
+    }
+  });
 
-//       result.push(rows[0]);
+  // TODO   signup    registration doesnot require authorization
 
-//       // Check if all queries have completed
-//       if (completedQueries === pids.length) {
-//         res.json(result);
-//       }
-//     });
-//   }
-// });
+  // TODO:   ====== AUTHORIZATION NEDED====== to perform these activities
+
+  // comment Mangement
+  app.post('/api/comments/add', async (req, res) => {
+  try {
+    res.send("pass [postId, commentContent, userId] : to create new comment for a post")
+  } catch(error) {
+    res.status(500).json({error: error.stack});
+  }
+  });
+
+  app.post('/api/comments/edit', async(req, res) => {
+    try {
+      res.send('send [commentId, userId] to edit your comment');
+    } catch(error) {
+      res.status(500).json({error: error.stack});
+    }
+  });
+
+  app.post('api/comments/delete', async(req, res) => {
+    try {
+      res.send('provide [commentId, userId] to delete your comment' )
+    } catch(error) {
+      res.status(500).json({error: error.stack});
+    }
+  });
+
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
